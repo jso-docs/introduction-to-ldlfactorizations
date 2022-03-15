f020a2df3bc14d4d096046abff75a8b0815ea686
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-03-15 22:44:43 +0100

    Copy tutorial from package repo

--- index.jl
+++ index.jl
@@ -1,34 +1,68 @@
-# # Template for jso-docs separate pages
-
-# \toc
-
-# Modify this file
-
-# ## Testing
-
-# Testing
-
-A = rand(5, 5)
-b = A * ones(5)
-x = A \ b
-
-# Testing
-
-using LinearAlgebra
-norm(A * x - b), norm(x .- 1)
-
 #=
-## Plotting
 
-Use `joinpath("__site/assets", filename)` as the path to save the image in the right place and use `{{ rfig filename.ext Caption of image }}` to add the image afterwards.
-It is a good idea to use `# hide` so the save command doesn't appear. See the example below.
+## A basic example
+
 =#
+n = 10
+A0 = rand(n, n)
+A = A0 * A0' + I # A is symmetric positive definite
+b = rand(n)
+#=
 
-using Plots
+We solve the system `A x = b` using LDLFactorizations.jl:
 
-x = rand(3)
-y = rand(3)
-plot(x, y)
-png(joinpath("__site/assets", "myplot")) # hide
+=#
+using LDLFactorizations, LinearAlgebra
+Au = Symmetric(triu(A), :U) # get upper triangle and apply Symmetric wrapper
+LDL = ldl(Au)
+x = LDL \ b
+#=
 
-# {{ rfig myplot.png Example of plot and description }}
+## A more performance-focused example
+
+We build a problem with sparse arrays.
+
+=#
+using SparseArrays
+n = 100
+# create create a SQD matrix A:
+A0 = sprand(Float64, n, n, 0.1)
+A1 = A0 * A0' + I
+A = [A1   A0;
+     A0' -A1]
+b = rand(2 * n)
+#=
+
+Now if we want to use the factorization to solve multiple systems that have 
+the same sparsity pattern as A, we only have to use `ldl_analyze` once.
+
+=#
+Au = Symmetric(triu(A), :U) # get upper triangle and apply Symmetric wrapper
+x = similar(b)
+
+LDL = ldl_analyze(Au) # symbolic analysis
+ldl_factorize!(Au, LDL) # factorization
+ldiv!(x, LDL, b) # solve in-place (we could use ldiv!(LDL, b) if we want to overwrite b)
+
+Au.data.nzval .+= 1.0 # modify Au without changing the sparsity pattern
+ldl_factorize!(Au, LDL) 
+ldiv!(x, LDL, b)
+#=
+
+## Dynamic Regularization
+
+When the matrix to factorize is (nearly) singular and the factorization encounters (nearly) zero pivots, 
+if we know the signs of the pivots and if they are clustered by signs (for example, the 
+`n_d` first pivots are positive and the other pivots are negative before permuting), we can use:
+
+=#
+ϵ = sqrt(eps())
+Au = Symmetric(triu(A), :U)
+LDL = ldl_analyze(Au)
+LDL.tol = ϵ
+LDL.n_d = 10
+LDL.r1 = 2 * ϵ # if any of the n_d first pivots |D[i]| < ϵ, then D[i] = sign(LDL.r1) * max(abs(D[i] + LDL.r1), abs(LDL.r1))
+LDL.r2 = -ϵ # if any of the n - n_d last pivots |D[i]| < ϵ, then D[i] = sign(LDL.r2) * max(abs(D[i] + LDL.r2), abs(LDL.r2))
+ldl_factorize!(Au, LDL)
+#=
+=#
